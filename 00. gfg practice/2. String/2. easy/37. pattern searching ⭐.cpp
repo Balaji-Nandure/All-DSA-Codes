793cc250385f11b30249dd255388d903fb3dc371
Balaji Nandure <balajirn.2016@gmail.com>
2026-05-27 02:19:00 +0530

    docs: remove obsolete configuration documentation files

--- 00. gfg practice/2. String/2. easy/37. pattern searching ⭐.cpp
+++ 00. gfg practice/2. String/2. easy/37. pattern searching ⭐.cpp
@@ -33,20 +33,20 @@ private:
     vector<int> computeLPS(const string &pat) {
         int m = pat.length();
         vector<int> lps(m, 0);
-        int len = 0;
-        int i = 1;
+        int pre = 0;
+        int suf = 1;
         
-        while (i < m) {
-            if (pat[i] == pat[len]) {
-                len++;
-                lps[i] = len;
-                i++;
+        while (suf < m) {
+            if (pat[suf] == pat[pre]) {
+                pre++;
+                lps[suf] = pre;
+                suf++;
             } else {
-                if (len != 0) {
-                    len = lps[len - 1];
+                if (pre != 0) {
+                    pre = lps[pre - 1];
                 } else {
-                    lps[i] = 0;
-                    i++;
+                    lps[suf] = 0;
+                    suf++;
                 }
             }
         }
